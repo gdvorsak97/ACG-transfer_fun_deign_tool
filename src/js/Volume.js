6f3ee702b1bd343eefb9a8921df6cf3aea345b05
Grega Dvoršak <gdvorsak97@gmail.com>
2020-05-26 23:10:35 +0200

    Basic histogram visualized, need to do this more pretty

--- src/js/Volume.js
+++ src/js/Volume.js
@@ -1,6 +1,7 @@
 // #package js/main
 
 // #include WebGL.js
+// #import * as d3 from "d3";
 
 class Volume {
 
@@ -16,6 +17,22 @@ constructor(gl, reader, options) {
     this.modalities = null;
     this.blocks     = null;
     this._texture   = null;
+}
+
+getValuesForHist(data) {
+    let scalar_values = [];
+    let gradient_magnitudes = [];
+    for (let i = 0; i < data.length; i++){
+        if (i%2 === 0){
+            scalar_values.push(data[i]);
+        } else if (i%2 === 1){
+            gradient_magnitudes.push(data[i])
+        }
+    }
+    return {
+        scalar: scalar_values,
+        gradient: gradient_magnitudes
+    };
 }
 
 readMetadata(handlers) {
@@ -70,6 +87,7 @@ readModality(modalityName, handlers) {
     }
     gl.texStorage3D(gl.TEXTURE_3D, 1, internalFormat, dimensions.width, dimensions.height, dimensions.depth);
     let remainingBlocks = modality.placements.length;
+    let usedData = null
     modality.placements.forEach(placement => {
         this._reader.readBlock(placement.index, {
             onData: data => {
@@ -81,12 +99,25 @@ readModality(modalityName, handlers) {
                     position.x, position.y, position.z,
                     blockdim.width, blockdim.height, blockdim.depth,
                     format, gl.UNSIGNED_BYTE, new Uint8Array(data));
+                usedData  = new Uint8Array(data)
                 remainingBlocks--;
-                console.log(data)
                 if (remainingBlocks === 0) {
                     this.ready = true;
                     handlers.onLoad && handlers.onLoad();
                 }
+                let values = this.getValuesForHist(usedData)
+                console.log(values.gradient);
+                let hist = d3.histogram(values.scalar);
+                console.log(hist)
+                let histData = [
+                    {
+                        x: values.scalar,
+                        y: values.gradient,
+                        type: 'histogram2d'
+                    }
+                ];
+                Plotly.newPlot('myDiv', histData);
+
             }
         });
     });
